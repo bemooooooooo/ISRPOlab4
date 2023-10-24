6bcd2c90d550907681664aae08d397a401020cd9
Name <you@example.com>
2023-10-24 19:36:28 +0300

    Added documentation for functions in tests and added class of test

--- rectangle.py
+++ rectangle.py
@@ -1,3 +1,5 @@
+import unittest
+
 def area(a, b):
     '''
     Calculates the area of rectangle
@@ -5,7 +7,7 @@ def area(a, b):
     :type a: int or float
     :param b: size of the side of the rectangle (adjacent to the first side)
     :type b: int or float
-    :rtype: int or float
+    :type: int or float
     :return: a*b - area of the rectangle
     :example: area(2, 4) = 8
     '''
@@ -18,8 +20,64 @@ def perimeter(a, b):
     :type a: int or float
     :param b: size of the side of the rectangle (adjacent to the first side)
     :type b: int or float
-    :rtype: int or float
+    :type: int or float
     :return: 2*(a+b) - perimeter of the rectangle
     example: perimeter(2, 4) = 12
     '''
     return 2*(a + b)
+
+
+
+class RectangleTestCase(unittest.TestCase):
+    '''
+    Tests the perimeter and area functions for rectangular
+    '''
+    def test_zero_area(self):
+        '''
+        Test zero multiply zero in area function
+        '''
+        res = area(0,0)
+        '''
+        Compares the output and the answer we calculated
+        '''
+        self.assertEqual(res, 0)
+
+    def test_square_area(self):
+        '''
+        Test if rectangle was a square
+        '''
+        res = area(10,10)
+        '''
+        Compares the output and the answer we calculated
+        '''
+        self.assertEqual(res, 100)
+
+    def test_zero_perimeter(self):
+        '''
+        Tets if a and b were zero
+        '''
+        res = perimeter(0,0)
+        '''
+        Compares the output and the answer we calculated
+        '''
+        self.assertEqual(res, 0)
+
+    def test_zero_one_perimeter(self):
+        '''
+        Test if a and b were pair of 0 and 1
+        '''
+        res = perimeter(0,1)
+        '''
+        Compares the output and the answer we calculated
+        '''
+        self.assertEqual(res, 2)
+
+    def test_square_perimeter(self):
+        '''
+        Test if rectangular was a square
+        '''
+        res = perimeter(10,10)
+        '''
+        Compares the output and the answer we calculated
+        '''
+        self.assertEqual(res, 40)
