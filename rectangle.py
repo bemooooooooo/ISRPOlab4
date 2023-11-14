3129b5b6de022525ba402175d0fa6ab43c067715
Name <you@example.com>
2023-11-14 07:22:34 +0300

    #

--- rectangle.py
+++ rectangle.py
@@ -1,3 +1,5 @@
+import unittest
+
 def area(a, b):
     '''
     Calculates the area of rectangle
@@ -23,3 +25,57 @@ def perimeter(a, b):
     example: perimeter(2, 4) = 12
     '''
     return 2*(a + b)
+
+class RectangleTestCase(unittest.TestCase):
+    '''
+    Tests the perimeter and area functions for rectangular
+    '''
+    def test_zero_area(self):
+        '''
+        Test zero multiply zero in area function
+        '''
+        res = area(0,0)
+        '''
+        Compares the output and the answer we calculated
+        '''
+        self.assertEqual(res, 0)
+
+    def test_square_area(self):
+        '''
+        Test if rectangle was a square
+        '''
+        res = area(10,10)
+        '''
+        Compares the output and the answer we calculated
+        '''
+        self.assertEqual(res, 100)
+
+    def test_zero_perimeter(self):
+        '''
+        Tets if a and b were zero
+        '''
+        res = perimeter(0,0)
+        '''
+        Compares the output and the answer we calculated
+        '''
+        self.assertEqual(res, 0)
+
+    def test_zero_one_perimeter(self):
+        '''
+        Test if a and b were pair of 0 and 1
+        '''
+        res = perimeter(0,1)
+        '''
+        Compares the output and the answer we calculated
+        '''
+        self.assertEqual(res, 2)
+
+    def test_square_perimeter(self):
+        '''
+        Test if rectangular was a square
+        '''
+        res = perimeter(10,10)
+        '''
+        Compares the output and the answer we calculated
+        '''
+        self.assertEqual(res, 40)
